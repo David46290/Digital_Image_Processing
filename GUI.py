61f03c5ee48deafc33ce60ee1e4ecb3e35306945
David46290 <141000324+David46290@users.noreply.github.com>
2024-11-30 15:13:50 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,49 +4,48 @@ import sys
 def event(label_, click_time_):
     click_time_ = click_time_ + 1
     label_.setText(str(click_time_)) 
+    return click_time_
     
-
-app = QtWidgets.QApplication(sys.argv)
-window_main = QtWidgets.QMainWindow()
-window_main.setObjectName("window_main")
-window_main.setWindowTitle("test")
-window_main.resize(1280, 720)
-
-button_push = QtWidgets.QPushButton(window_main)
-button_push.setGeometry(400, 200, 400, 200) # (x, y, w, h)
-button_push.setObjectName("button")
-button_push.setText("Add Number")
-button_push.setStyleSheet('''
-                            QPushButton
-                            {
-                                font-size:20px;
-                                color: blue;
-                                background: cyan;
-                                border: 10px solid #000;
-                            }
-                            QPushButton:hover
-                            {
-                                color: yellow;
-                                background: red;
-                            }
+if __name__ == '__main__':
+    click_time = 0
+    
+    app = QtWidgets.QApplication(sys.argv)
+    window_main = QtWidgets.QMainWindow()
+    window_main.setObjectName("window_main")
+    window_main.setWindowTitle("test")
+    window_main.resize(1280, 720)
+    
+    button_push = QtWidgets.QPushButton(window_main)
+    button_push.setGeometry(400, 200, 400, 200) # (x, y, w, h)
+    button_push.setObjectName("button")
+    button_push.setText("Add Number")
+    button_push.setStyleSheet('''
+                                QPushButton
+                                {
+                                    font-size:20px;
+                                    color: blue;
+                                    background: cyan;
+                                    border: 10px solid #000;
+                                }
+                                QPushButton:hover
+                                {
+                                    color: yellow;
+                                    background: red;
+                                }
+                            ''')
+                            
+    label = QtWidgets.QLabel(window_main)
+    label.setText(str(click_time))
+    label.setStyleSheet('''
+                        QLabel
+                        {
+                            font-size:15px;
+                            color: green;
+                        }
                         ''')
-                        
-label = QtWidgets.QLabel(window_main)
-label.setText('0')
-label.setStyleSheet('''
-                    QLabel
-                    {
-                        font-size:15px;
-                        color: green;
-                    }
-                    ''')
-label.setGeometry(200, 100, 200, 150)
-
-click_time = 0
-
-button_push.clicked.connect(lambda:event(label, click_time))
-
-
-
-window_main.show()
-sys.exit(app.exec_())
+    label.setGeometry(200, 100, 200, 150)
+    
+    button_push.clicked.connect(event(label, click_time))
+    
+    window_main.show()
+    sys.exit(app.exec_())
